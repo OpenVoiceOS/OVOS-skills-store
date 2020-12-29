f928ddc4e123e2142cd83c002c228b346a2b1dbe
jarbasal <jarbasai@mailfence.com>
2020-12-29 13:25:17 +0000

    json_feed

--- gen_skill_list.py
+++ gen_skill_list.py
@@ -2,10 +2,18 @@ from os import listdir
 from os.path import dirname
 import json
 
-output = []
+output = {
+    "version": "https://jsonfeed.org/version/1",
+    "title": "OpenVoiceOS skills store",
+    "description": "Welcome to OpenVoiceOS skills store",
+    "home_page_url": "https://openvoiceos.github.io/OVOS-skills-store",
+    "feed_url": "https://openvoiceos.github.io/OVOS-skills-store/skill_list",
+    "items": []
+}
+
 for skill in [f for f in listdir(dirname(__file__)) if f.endswith(".json")]:
     with open(skill) as f:
-        output.append(json.load(f))
+        output["items"].append(json.load(f))
 
 with open("skill_list", "w") as f:
     json.dump(output, f, indent=4)
