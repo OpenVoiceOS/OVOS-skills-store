1b28199ea7cf6b0b98939228504f06bcc56cb833
Daniel McKnight <34697904+NeonDaniel@users.noreply.github.com>
2021-05-06 06:32:58 -0700

    Fix GitHub pages automation (#15)

--- gen_skill_list.py
+++ gen_skill_list.py
@@ -1,5 +1,5 @@
 from os import listdir, getcwd
-from os.path import dirname
+from os.path import dirname, join
 import json
 
 output = {
@@ -10,10 +10,10 @@ output = {
     "feed_url": "https://openvoiceos.github.io/OVOS-skills-store/skills.json",
     "items": []
 }
-dir_to_check = dirname(__file__) or getcwd()
+dir_to_check = dirname(__file__) or join(getcwd(), "main")
 for skill in [f for f in listdir(dir_to_check) if f.endswith(".json")]:
-    with open(skill) as f:
+    with open(join(dir_to_check, skill)) as f:
         output["items"].append(json.load(f))
 
-with open("skills.json", "w") as f:
+with open(join(dir_to_check, "skills.json"), "w") as f:
     json.dump(output, f, indent=4)
