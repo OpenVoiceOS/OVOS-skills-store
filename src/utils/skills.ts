c4b57de7580442c4252bb5d0409d61c221504776
suvanbanerjee <banerjeesuvan@gmail.com>
2025-03-02 23:48:50 +0530

    new reading logic

--- src/utils/skills.ts
+++ src/utils/skills.ts
@@ -1,11 +1,4 @@
 export async function getSkills() {
-  const skillFiles = import.meta.glob('../data/skills/*.json');
-  const skills = [];
-
-  for (const path in skillFiles) {
-    const skill = await skillFiles[path]();
-    skills.push(skill.default);
-  }
-
-  return skills;
+  const skillsData = await import('../../skills.json');
+  return skillsData.default.items;
 }
